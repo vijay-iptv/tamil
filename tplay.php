651eb3b8e66f756d017d460426ac268a55ec012d
vijay-iptv <167630289+vijay-iptv@users.noreply.github.com>
2025-11-29 21:26:24 +0530

    asdfasfdsdf
    asdfasdf

--- tplay.php
+++ tplay.php
@@ -1,7 +1,7 @@
 #EXTM3U x-tvg-url="https://avkb.short.gy/epg.xml.gz" 
 #EXTINF:-1 tvg-id="ts842" tvg-logo="https://ltsk-cdn.s3.eu-west-1.amazonaws.com/jumpstart/Temp_Live/cdn/HLS/Channel/transparentImages/Tata-Play-Darshan-Transparent.png" group-title="Devotional",Shirdi Sai Baba
 #KODIPROP:inputstream.adaptive.license_type=clearkey
-#KODIPROP:inputstream.adaptive.license_key=https://tp.drmlive-01.workers.dev?id=723
+#KODIPROP:inputstream.adaptive.license_key=https://tp.drmlive-01.workers.dev?id=411
 #KODIPROP:inputstream.adaptive.manifest_type=mpd
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36
-https://delta45tatasky.akamaized.net/out/i/723.mpd?hdnea=st=1764431177~exp=1764431777~acl=/*~id=1464687407~hmac=bc68bd5fbd34fba472f4dd68065088ee7d0c25b8a35e468cafba19703e42cbe2|X-Forwarded-For=59.178.74.184&Origin=https://watch.tataplay.com&Referer=https://watch.tataplay.com/
+https://bpaita6.akamaized.net/bpk-tv/irdeto_com_Channel_644/output/manifest.mpd?hdnea=st=1764431702~exp=1764432302~acl=/bpk-tv/irdeto_com_Channel_644/output/*~id=1464687407~hmac=22df988ca605dec54254ebcbc13880793d37b78462fbe13cad2c96fe2806c8e9
