ffb7f41c94b2badf17e525238f986ca4413fff80
vijay-iptv <167630289+vijay-iptv@users.noreply.github.com>
2025-11-29 21:12:34 +0530

    asdfasdfa
    asdfasdf

--- tplay.php
+++ tplay.php
@@ -4,4 +4,4 @@
 #KODIPROP:inputstream.adaptive.license_key=https://tp.drmlive-01.workers.dev?id=842
 #KODIPROP:inputstream.adaptive.manifest_type=mpd
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36
-https://delta45tatasky.akamaized.net/out/i/723.mpd?__hdnea__=st=1764430451~exp=1764431051~acl=/*~hmac=a592632aa316ecdbc1e95b50f93ec5c38b65e64d394047395fcfc56d7b811166&xxx=%7Ccookie=__hdnea__=st=1764430451~exp=1764431051~acl=/*~hmac=a592632aa316ecdbc1e95b50f93ec5c38b65e64d394047395fcfc56d7b811166
+https://delta45tatasky.akamaized.net/out/i/723.mpd?hdnea=st=1764430937~exp=1764431537~acl=/*~id=1464687407~hmac=8b50b9ee008cb4d4778ef2f73772d08557e1739949328402192e27763af7c7d7
