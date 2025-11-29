cd221a82a67834bdd83a5f794cd85158432feb6d
vijay-iptv <167630289+vijay-iptv@users.noreply.github.com>
2025-11-29 21:16:57 +0530

    asdfasdfdsf
    asdfasdf

--- tplay.php
+++ tplay.php
@@ -1,7 +1,7 @@
 #EXTM3U x-tvg-url="https://avkb.short.gy/epg.xml.gz" 
 #EXTINF:-1 tvg-id="ts842" tvg-logo="https://ltsk-cdn.s3.eu-west-1.amazonaws.com/jumpstart/Temp_Live/cdn/HLS/Channel/transparentImages/Tata-Play-Darshan-Transparent.png" group-title="Devotional",Shirdi Sai Baba
 #KODIPROP:inputstream.adaptive.license_type=clearkey
-#KODIPROP:inputstream.adaptive.license_key=https://tp.drmlive-01.workers.dev?id=842
+#KODIPROP:inputstream.adaptive.license_key=https://tp.drmlive-01.workers.dev?id=723
 #KODIPROP:inputstream.adaptive.manifest_type=mpd
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36
-https://delta45tatasky.akamaized.net/out/i/723.mpd?hdnea=st=1764430937~exp=1764431537~acl=/*~id=1464687407~hmac=8b50b9ee008cb4d4778ef2f73772d08557e1739949328402192e27763af7c7d7
+https://delta45tatasky.akamaized.net/out/i/723.mpd?hdnea=st=1764431177~exp=1764431777~acl=/*~id=1464687407~hmac=bc68bd5fbd34fba472f4dd68065088ee7d0c25b8a35e468cafba19703e42cbe2|X-Forwarded-For=59.178.74.184&Origin=https://watch.tataplay.com&Referer=https://watch.tataplay.com/
